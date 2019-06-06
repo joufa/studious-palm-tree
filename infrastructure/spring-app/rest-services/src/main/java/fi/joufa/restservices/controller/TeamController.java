c139a57b6de27d10fdbe94bf31424d0c634c57f6
Antti <antti.reijonen@combitech.fi>
2019-06-06 15:55:26 +0300

    Team-feature code

--- infrastructure/spring-app/rest-services/src/main/java/fi/joufa/restservices/controller/TeamController.java
+++ infrastructure/spring-app/rest-services/src/main/java/fi/joufa/restservices/controller/TeamController.java
@@ -1,10 +1,12 @@
 package fi.joufa.restservices.controller;
 
+import fi.joufa.agileservices.exceptions.AgileException;
 import fi.joufa.agileservices.services.TeamService;
 import fi.joufa.domain.model.Team;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,10 @@ public class TeamController {
   @GetMapping(path = "/teams")
   public List<Team> findAll() {
     return teamService.findAll();
+  }
+
+  @GetMapping(path = "/teams/{id}")
+  public Team findOne(@PathVariable("id") Long id) throws AgileException {
+    return teamService.findTeamById(id);
   }
 }
